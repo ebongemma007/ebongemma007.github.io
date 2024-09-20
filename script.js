7f3610f825da49cd50dc962cb3ad9660f6bcc63b
ebongemma007 <emmanuelebong007@gmail.com>
2024-09-20 13:54:48 +0100

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-// Theme Toggle
-const toggleButton = document.getElementById("theme-toggle");
-const currentTheme = localStorage.getItem("theme");
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-}
-
-toggleButton.addEventListener("click", function() {
-  let theme = document.documentElement.getAttribute("data-theme");
-  if (theme === "dark") {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-  } else {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-  }
-});
-
-// Contact Form Submission
-document.getElementById("contact-form").addEventListener("submit", function(event) {
+document.getElementById("contact-form").addEventListener("submit", function(event){
   event.preventDefault();
   alert("Message sent successfully!");
 });
